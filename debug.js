006ae421adf283159fea12051040880948c6521f
Jonathan Lipps <jlipps@gmail.com>
2022-05-05 09:39:23 -0700

    fix: fix devtools extension install

--- debug.js
+++ debug.js
@@ -3,15 +3,19 @@ const isDev = process.env.NODE_ENV === 'development';
 export async function installExtensions () {
   if (isDev) {
     const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
-    const extensions = [
-      'REACT_DEVELOPER_TOOLS',
-      'REDUX_DEVTOOLS'
-    ];
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    for (const name of extensions) {
-      try {
-        await installer.default(installer[name], forceDownload);
-      } catch (e) {} // eslint-disable-line
+    const { app } = require('electron');
+    const { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = installer;
+    const opts = {
+      forceDownload: !!process.env.UPGRADE_EXTENSIONS,
+      loadExtensionOptions: {
+        allowFileAccess: true
+      }
+    };
+    await app.whenReady();
+    try {
+      await installer.default([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], opts);
+    } catch (e) {
+      console.warn(`Error installing extension: ${e}`);
     }
   }
 }
